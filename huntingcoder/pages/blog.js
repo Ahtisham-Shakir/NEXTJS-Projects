58b66ba7151b6f6f8340b724ef577fee8c35bef8
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2022-10-22 20:30:21 +0500

    worked on blog page blogPost page and created blogs and getblog endpoints

--- huntingcoder/pages/blog.js
+++ huntingcoder/pages/blog.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Link from "next/link";
+import styles from "../styles/Blog.module.css";
 
 const Blog = () => {
-  return <div>blog</div>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h2>Popular Blogs</h2>
+        <div>
+          <Link href={"/blogpost/learn-javaScript"}>
+            <h3 className={styles.blogItemh3}>Blog Title</h3>
+          </Link>
+          <p>Blog description</p>
+        </div>
+        <div className="blogItems">
+          <h3>Blog Title</h3>
+          <p>Blog description</p>
+        </div>
+        <div className="blogItems">
+          <h3>Blog Title</h3>
+          <p>Blog description</p>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Blog;
--- huntingcoder/pages/blogpost/[slug].js
+++ huntingcoder/pages/blogpost/[slug].js
@@ -1,10 +1,29 @@
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "../../styles/BlogPost.module.css";
 
 const Slug = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>slug : {slug}</div>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>This is the title of the post {slug}</h1>
+        <hr />
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </p>
+      </main>
+    </div>
+  );
 };
 
 export default Slug;
