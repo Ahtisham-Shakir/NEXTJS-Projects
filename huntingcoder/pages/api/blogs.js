c10f5355bbda3e05eefe4080482dfb8b058be79e
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2022-10-22 21:33:55 +0500

    fetching blogs from api route and displaying on frontend

--- huntingcoder/pages/api/blogs.js
+++ huntingcoder/pages/api/blogs.js
@@ -1,8 +1,13 @@
 import * as fs from "fs";
 
-export default function handler(req, res) {
-  fs.readdir("blogdata", (err, data) => {
-    console.log(data);
-    res.status(200).json(data);
-  });
+export default async function handler(req, res) {
+  let myFile;
+  let allBlogs = [];
+  let data = await fs.promises.readdir("blogdata");
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    myFile = await fs.promises.readFile("blogdata/" + item, "utf-8");
+    allBlogs.push(JSON.parse(myFile));
+  }
+  res.status(200).json(allBlogs);
 }
--- huntingcoder/pages/blog.js
+++ huntingcoder/pages/blog.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Blog.module.css";
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/blogs")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h2>Popular Blogs</h2>
-        <div>
-          <Link href={"/blogpost/learn-javaScript"}>
-            <h3 className={styles.blogItemh3}>Blog Title</h3>
-          </Link>
-          <p>Blog description</p>
-        </div>
-        <div className="blogItems">
-          <h3>Blog Title</h3>
-          <p>Blog description</p>
-        </div>
-        <div className="blogItems">
-          <h3>Blog Title</h3>
-          <p>Blog description</p>
-        </div>
+        {blogs.map((item) => (
+          <div key={item.slug}>
+            <Link href={`/blogpost/${item.slug}`}>
+              <h3 className={styles.blogItemh3}>{item.title}</h3>
+            </Link>
+            <p className={styles.blogItemp}>{item.content.substr(0, 120)}...</p>
+          </div>
+        ))}
       </main>
     </div>
   );
