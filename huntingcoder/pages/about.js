88a77d8d748848a8078f0913249596c57f937133
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2022-10-29 20:17:40 +0500

    worked on about page

--- huntingcoder/pages/about.js
+++ huntingcoder/pages/about.js
@@ -1,7 +1,36 @@
 import React from "react";
+import styles from "../styles/About.module.css";
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.center}>About Hunting Coder</h1>
+      <h2>Introduction</h2>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <h2>Services Offered</h2>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </p>
+      <ul>
+        <li>Hunt Developer</li>
+        <li>Digital Marketing</li>
+        <li>Content Writing</li>
+      </ul>
+    </div>
+  );
 };
 
 export default About;
