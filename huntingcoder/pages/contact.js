e0a01f43850313de8a7640c59b06d73fc536ce97
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2022-10-29 19:23:20 +0500

    created contact us page and receiving data through api

--- huntingcoder/pages/contact.js
+++ huntingcoder/pages/contact.js
@@ -1,7 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../styles/Contact.module.css";
 
 const Contact = () => {
-  return <div>contact</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    desc: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    fetch("http://localhost:3000/api/postContact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => {
+        alert("Thanks for Contacting us!");
+        setFormData({
+          name: "",
+          phone: "",
+          email: "",
+          desc: "",
+        });
+      })
+      .catch((err) => {
+        alert("Some internal error try again later");
+      });
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Contact</h1>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.mb3}>
+          <label htmlFor="name" className={styles.formLabel}>
+            Enter Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            aria-describedby="emailHelp"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="email" className={styles.formLabel}>
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="phone" className={styles.formLabel}>
+            Enter Phone
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="phone"
+            aria-describedby="emailHelp"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-floating">
+          <label htmlFor="floatingTextarea2">Comments</label>
+          <textarea
+            className="form-control"
+            placeholder="Leave a comment here"
+            id="floatingTextarea2"
+            name="desc"
+            value={formData.desc}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
